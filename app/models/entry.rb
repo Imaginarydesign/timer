0331b0ef918b9a320595f94add813fd3d62d6198
Adam Koniuszewski <adam@imaginarydesign.co.uk>
2014-11-17 22:35:25 +0000

    Add entries validation

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,5 +1,10 @@
 class Entry < ActiveRecord::Base
   belongs_to :employee
 
+  # Validation
   validates :employee_id, presence: true
+  validates :name,
+                  length: { minimum: 4 },
+                  presence: true
+                  
 end
