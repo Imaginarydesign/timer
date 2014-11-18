8d451cdcaf1e11ab8be66c4c5a8910b28d56f239
Adam Koniuszewski <adam@imaginarydesign.co.uk>
2014-11-18 14:46:19 +0000

    Add project creation validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,9 @@
 class Project < ActiveRecord::Base
   has_and_belongs_to_many :employee
   has_many :entries
+
+  # Validation
+  validates :name,
+                  length: { minimum: 4 },
+                  presence: true
 end
