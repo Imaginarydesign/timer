6a5754376876fe56fa0a88c37681ba159f7729da
Adam Koniuszewski <adam@imaginarydesign.co.uk>
2014-11-23 06:29:49 +0000

    Add bootstrap

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -78,6 +78,6 @@ class EntriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def entry_params
-      params.require(:entry).permit(:name, :time, :employee_id)
+      params.require(:entry).permit(:name, :time, :employee_id, :project_id)
     end
 end
