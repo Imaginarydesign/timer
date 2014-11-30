1415f7aeafddf76d5afb036718159a21418db377
Adam Koniuszewski <adam@imaginarydesign.co.uk>
2014-11-30 01:09:29 +0000

    Add labels

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141129233615) do
+ActiveRecord::Schema.define(version: 20141130002759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20141129233615) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "project_id"
+    t.integer  "billable"
   end
 
   add_index "entries", ["employee_id"], name: "index_entries_on_employee_id", using: :btree
