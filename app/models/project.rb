02cf1a681f6d9eb1b4e2272f65eeca266c738a9b
Adam Koniuszewski <adam@imaginarydesign.co.uk>
2014-12-03 20:57:29 +0000

    Fix error preventing project to be deleted

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,10 +1,6 @@
 class Project < ActiveRecord::Base
-  has_and_belongs_to_many :employees
+  has_many :employees
   has_many :entries
-
-  # def total_time
-  #   self.entries.sum(:time)
-  # end
 
   # Validation
   validates :name,
