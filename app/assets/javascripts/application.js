3f5a8f5ed8b3a46725163fb14b241a599e7982fd
Adam Koniuszewski <adam@imaginarydesign.co.uk>
2014-12-05 04:00:45 +0000

    Update how bootstrap js in imported into js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap/bootstrap
+//= require bootstrap-sprockets
 //= require bootstrap-datepicker/core
 //= require_tree .
 
